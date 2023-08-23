9560e2dc55775a8990ffe26d221ad9967ff9b646
gchkadua3 <120250936+gchkadua3@users.noreply.github.com>
2023-08-23 14:53:20 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const apiUrl = 'https://fakestoreapi.com/products';
+
+axios.get(apiUrl).then(response => {
+    const products = response.data;
+    const cardContainer = document.querySelector('.card-container');
+    for(let i = 0; i < 8; i++) { 
+        const card = document.createElement('div');
+        card.classList.add('card');
+        const product = products[i];
+
+        card.innerHTML = `
+        <img src=${product.image}>
+        <p class="description">${product.title}</p>
+        <p class="price">Price: ${product.price}</p>
+        `;
+
+        cardContainer.appendChild(card);
+    }
+});
+
+
+axios.get(apiUrl).then(response => {
+    const products = response.data;
+    const cardContainer2 = document.querySelector('.card-container2');
+    for(let i = 0; i < 4; i++) { 
+        const card = document.createElement('div');
+        card.classList.add('card');
+        const product = products[i];
+
+        card.innerHTML = `
+        <img src=${product.image}>
+        <p class="description">${product.title}</p>
+        <p class="price">Price: ${product.price}</p>
+        `;
+
+        cardContainer2.appendChild(card);
+    }
+})
